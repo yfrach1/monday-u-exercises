1d8179499a589baa3be148d8e0a8293355cd3d1b
Harel <harel@h-MBP-sl-Harel.lan>
2022-05-19 16:19:00 +0300

    add sort button

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -1,5 +1,5 @@
 let tasks_list = [];
-
+let tasks_amount = 0;
 const add_button = document.getElementById("add_button");
 add_button.addEventListener("click", onClickAddTask);
 
@@ -10,38 +10,57 @@ input.addEventListener("keypress", (event) => {
   }
 });
 
+const sort_butten = document.getElementById("sort_button");
+sort_butten.addEventListener("click", sortTasksList);
+
 const clearAll_button = document.getElementById("clearAll_button");
 clearAll_button.addEventListener("click", onClickClearAll);
 
 function onClickAddTask() {
   let input_tag = document.getElementById("task_input");
-  let value = input_tag.value;
-  if (!input_tag.value) {
+  let text = input_tag.value;
+  if (!text) {
     alert("An empty task is for lazy pepole ;)");
     return;
+  } else if (firstCharIsNotCapital(text)) {
+    text = capitalizeFirstLetter(text);
   }
-  if (tasks_list.length === 0) {
-    setEmptyTasksDivDisplay("none");
-  }
+
   input_tag.value = "";
-  tasks_list.push(value);
-  injectNewTaskToHtml(value);
-  if (tasks_list.length === 1) {
+  if (tasks_amount === 0) {
+    setSortButtonDisplay("block");
+    setEmptyTasksDivDisplay("none");
     setBottomDivBarDisplay("flex");
     setTasksDivContainerDisplay("flex");
   }
-  updatePendingTask();
+  injectNewTaskToHtml(text);
+}
+
+function firstCharIsNotCapital(text) {
+  return text[0].toUpperCase() !== text[0];
+}
+
+function capitalizeFirstLetter(text) {
+  const capitalized = text.charAt(0).toUpperCase() + text.slice(1);
+  return capitalized;
 }
 
 function onClickClearAll() {
+  clearTasksHtml();
+  resetTaskList();
+}
+
+function clearTasksHtml() {
   const tasks_list_div = document.getElementById("tasks_list_container");
   tasks_list_div.innerHTML = "";
-  resetTaskList();
 }
 function resetTaskList() {
   tasks_list = [];
-  setBottomDivBarDisplay("none");
+  tasks_amount = 0;
+  setSortButtonDisplay("none");
   setEmptyTasksDivDisplay("block");
+  setBottomDivBarDisplay("none");
+  setTasksDivContainerDisplay("none");
 }
 
 function setBottomDivBarDisplay(displayStatus) {
@@ -56,23 +75,63 @@ function setEmptyTasksDivDisplay(displayStatus) {
   const tasks_list_div = document.getElementById("empty_tasks_text");
   tasks_list_div.style.display = displayStatus;
 }
+function setSortButtonDisplay(displayStatus) {
+  const sort_button = document.getElementById("sort_button");
+  sort_button.style.display = displayStatus;
+}
 
 function updatePendingTask() {
-  let taskAmount = tasks_list.length;
   const pending_tasks_p_tag = document.getElementById(
     "amount_of_pending_tasks"
   );
-  pending_tasks_p_tag.innerText = "You have " + taskAmount + " pending tasks";
+  pending_tasks_p_tag.innerText = "You have " + tasks_amount + " pending tasks";
 }
 
-function deleteTask(target) {
-  console.log("inside");
-  console.log(target);
+function sortTasksList() {
+  clearTasksHtml();
+  console.log(tasks_list);
+  tasks_list.sort((a, b) => {
+    return a.text.toLowerCase().localeCompare(b.text.toLowerCase());
+  });
+  const task_list_div = document.getElementById("tasks_list_container");
+  console.log(tasks_list);
+  tasks_list.forEach((object) => {
+    task_list_div.appendChild(object.element);
+  });
+}
+
+function removeTaskFromList(task) {
+  const index = tasks_list.findIndex((object) => {
+    return object.text === task;
+  });
+
+  tasks_list.splice(index, 1);
+  updateTasksAmount("-");
+  if (tasks_amount === 0) {
+    setSortButtonDisplay("none");
+    setEmptyTasksDivDisplay("block");
+    setBottomDivBarDisplay("none");
+    setTasksDivContainerDisplay("none");
+  }
+  updatePendingTask();
+}
+
+function updateTasksAmount(action) {
+  switch (action) {
+    case "+": {
+      tasks_amount++;
+      break;
+    }
+    case "-": {
+      tasks_amount--;
+      break;
+    }
+  }
 }
 
 function injectNewTaskToHtml(task) {
-  updatePendingTask();
   const task_div_tag = document.createElement("div");
+
   task_div_tag.className = "task_container";
   const task_p_tag = document.createElement("p");
 
@@ -82,9 +141,11 @@ function injectNewTaskToHtml(task) {
   delete_task_button.className = "delete_task_button";
   delete_task_button.id = "delete_task_button";
   delete_task_button.src = "../assets/svg/delete.svg";
-  delete_task_button.addEventListener("click", (target) => {
-    console.log(target.target);
+
+  delete_task_button.addEventListener("click", (event) => {
+    removeTaskFromList(task);
     task_div_tag.remove();
+    event.stopPropagation();
   });
 
   task_p_tag.innerText = task;
@@ -92,6 +153,15 @@ function injectNewTaskToHtml(task) {
   task_div_tag.appendChild(task_p_tag);
   task_p_tag.appendChild(delete_task_button);
 
-  const task_list_div = document.getElementById("tasks_list_container");
-  task_list_div.appendChild(task_div_tag);
+  const tasks_list_div = document.getElementById("tasks_list_container");
+
+  let newObj = { text: task, element: task_div_tag };
+  tasks_list.push(newObj);
+  updateTasksAmount("+");
+  updatePendingTask();
+  tasks_list_div.appendChild(task_div_tag);
+
+  task_div_tag.addEventListener("click", () => {
+    alert(task_p_tag.innerText);
+  });
 }
