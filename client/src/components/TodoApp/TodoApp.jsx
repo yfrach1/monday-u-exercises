c459d3d2cb860d48215ce4c49901d68172a62606
Harel Yfrach <34807437+yfrach1@users.noreply.github.com>
2022-07-18 14:28:23 +0300

    Update TodoApp.jsx

--- client/src/components/TodoApp/TodoApp.jsx
+++ client/src/components/TodoApp/TodoApp.jsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import styles from "./TodoApp.module.css";
-import MyToast from "../MyToast/MyToast";
+import MyToast from "../myToast/MyToast";
 import PropTypes from "prop-types";
-import OpacityLoading from "../Loader/Loader";
-import ListItemsConnector from "../ListItems/ListItems-connector";
+import OpacityLoading from "../loader/Loader";
+import ListItemsConnector from "../listItems/ListItems-connector";
 import UserInputConnector from "../UserInput/UserInput-connector";
 import ClearAllButtonConnector from "../Buttons/ClearAllButton-connector";
-import DisplayOptionConnector from "../DisplayOption/DisplayOption-connector";
+import DisplayOptionConnector from "../displayOption/DisplayOption-connector";
 
 const TodoApp = ({
   showLoader,
   showToast,
   toastParam,
   viewItemsAmount,
-  allItemsAmount,
+  allItesAmount,
+  fetchDataAction,
   hideToastAction,
   filter,
 }) => {
+  useEffect(() => {
+    fetchDataAction();
+  }, []);
+
   return (
     <div className={styles.contentPlacement}>
       <div className={styles.appContainer}>
@@ -31,7 +36,7 @@ const TodoApp = ({
         <DisplayOptionConnector />
 
         <ListItemsConnector />
-        {allItemsAmount ? (
+        {allItesAmount ? (
           <div className={styles.buttomBarContainer}>
             <span>
               {`The amount of ${filter} tasks is: ${viewItemsAmount}`}
@@ -49,7 +54,7 @@ TodoApp.propTypes = {
   showToast: PropTypes.bool,
   toastParam: PropTypes.object,
   viewItemsAmount: PropTypes.number,
-  allItemsAmount: PropTypes.number,
+  allItesAmount: PropTypes.number,
   fetchDataAction: PropTypes.func,
   hideToastAction: PropTypes.func,
   filter: PropTypes.string,
@@ -60,7 +65,7 @@ TodoApp.defaultProps = {
   showToast: false,
   toastParam: { toastType: "", message: "" },
   viewItemsAmount: 0,
-  allItemsAmount: 0,
+  allItesAmount: 0,
   fetchDataAction: () => {},
   hideToastAction: () => {},
   filter: "all",
