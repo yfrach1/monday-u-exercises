bc73adac1d3c0f652e8c84085f086861dafb9afa
Harel Yfrach <34807437+yfrach1@users.noreply.github.com>
2022-07-18 13:38:06 +0300

    Update items-view-selectors.js

--- client/src/app/selectors/items-view-selectors.js
+++ client/src/app/selectors/items-view-selectors.js
@@ -1,7 +1,3 @@
-import sortType from "../actions/constants/SortType";
-import filterType from "../constant/Filter";
-import { getItemsObj } from "./items-entities-selectors";
-import { createSelector } from "@reduxjs/toolkit";
 const getItemsView = (state) => state.itemsView;
 
 export const getShowLoader = (state) => getItemsView(state).showLoader;
@@ -10,64 +6,3 @@ export const getToastParam = (state) => getItemsView(state).toastParam;
 export const getItemsFilter = (state) => getItemsView(state).itemsFilter;
 export const getSortType = (state) => getItemsView(state).sortType;
 export const getSearchKey = (state) => getItemsView(state).searchKey;
-export const getInputValue = (state) => getItemsView(state).inputValue;
-
-const compareItemName = (a, b) => {
-  if (a.itemName < b.itemName) {
-    return -1;
-  }
-  if (a.itemName > b.itemName) {
-    return 1;
-  }
-  return 0;
-};
-
-export const getFilteredItems = createSelector(
-  [getItemsObj, getItemsFilter, getSortType, getSearchKey],
-  (itemsObj, filter, sort, searchKey) => {
-    let itemsToBeView = Object.keys(itemsObj).map((key) => itemsObj[key]);
-    switch (filter) {
-      case filterType.DONE: {
-        itemsToBeView = itemsToBeView.filter((item) => item.status);
-        break;
-      }
-      case filterType.PENDING: {
-        itemsToBeView = itemsToBeView.filter((item) => !item.status);
-        break;
-      }
-      default: {
-        break;
-      }
-    }
-
-    switch (sort) {
-      case sortType.DESC: {
-        itemsToBeView.sort(compareItemName);
-        break;
-      }
-      case sortType.ASC: {
-        itemsToBeView.sort(compareItemName).reverse();
-        break;
-      }
-      default: {
-        break;
-      }
-    }
-    if (searchKey) {
-      itemsToBeView = itemsToBeView.filter((item) =>
-        item.itemName.toLowerCase().includes(searchKey)
-      );
-    }
-
-    return itemsToBeView;
-  }
-);
-
-export const getViewItemsAmount = createSelector(
-  [getFilteredItems],
-  (itemsToBEView) => itemsToBEView.length
-);
-
-export const getAllItemsAmount = createSelector([getItemsObj], (itemsObj) => {
-  return Object.keys(itemsObj).length;
-});
