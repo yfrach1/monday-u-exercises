3224e5d2f463eaccceda6b6ddf6c0a1b9828ec8d
yfrach1 <yfrach1@gmail.com>
2022-07-18 18:35:58 +0300

    remove statistics

--- client/src/components/statistics/Statistics.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Statistics.module.css";
-
-const Statistics = () => {
-  return <div className={styles.contentPlacement}>Coming soon...</div>;
-};
-
-export default Statistics;
